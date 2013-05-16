bf1b6ee630f125a502f109e64a4035309dc97dce
myss <pdmyss@gmail.com>
2013-05-16 23:02:03 +0400

    меню почти готова

--- infa/models.py
+++ infa/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Name(models.Model):
     name = models.CharField(max_length=30)
 
-class People(models.Model):
-    fam = models.CharField(max_length=30)
-    im = models.CharField(max_length=30)
-    otv = models.CharField(max_length=30)
+class uchet(models.Model):
+    klient = models.CharField(max_length=30)
+    agent = models.CharField(max_length=30)
+    proc = models.CharField(max_length=100)
+    addtime_date = models.DateTimeField()
+
--- main.py
+++ main.py
@@ -2,18 +2,30 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from infa.models import Name, People
+from infa.models import Name, uchet
+import datetime
 
-def save(fio):
-    n = Name.object.create(name=fio)
+def save(klient, agent, proc, addtime ):
+    n = uchet(klient=klient, agent=agent, proc=proc, addtime_date=addtime)
     n.save()
 
 def menu(request):
-    p = People.objects.all()
     if request.method == 'POST':
-        fio = request.REQUEST['fio']
-        save(fio)
-    return render_to_response('menu.html')    
+        klient = request.POST['klient']
+        agent = request.POST['agent']
+        proc = request.POST['proc']
+        select_month = request.POST['select_month']
+        select_day = request.POST['select_day']
+        hourse = request.POST['hourse']
+        minut = request.POST['minut']
+        try:
+            addtime = datetime.datetime(2013, int(select_month), int(select_day), int(hourse), int(minut))                             
+            save(klient, agent, proc, addtime)
+        except:
+            pass
+
+    p = uchet.objects.all()
+    return render_to_response('menu.html', {'infa': p})
 
 def list1(request):
     return render_to_response('list1.html')
