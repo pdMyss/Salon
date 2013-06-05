30e26bbae6ffd1773b886c40d8cbea89a971af4c
myss <pdmyss@gmail.com>
2013-06-05 19:28:03 +0400

    Fix templates

--- uchet/settings.py
+++ uchet/settings.py
@@ -12,6 +12,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
+
 DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgres://postgres:12345@localhost/baza'
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
@@ -105,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/guru/Salon/salon/uchet/templates"
+    os.path.join(PROJECT_ROOT, "uchet", "templates"),
 )
 
 INSTALLED_APPS = (
