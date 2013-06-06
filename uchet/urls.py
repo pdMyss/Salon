f87439df4afb436c78a6dbfbe0da231d9d0e9ef0
myss <pdmyss@gmail.com>
2013-06-06 15:47:59 +0400

    Static

--- uchet/urls.py
+++ uchet/urls.py
@@ -23,3 +23,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
